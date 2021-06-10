1b8d3cbe8c6736c46a40ab62f70595fb7a36931f
Xaver Pinero <xaver.pinero@gmail.com>
2021-06-09 21:24:24 -0400

    monolith

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {useState, useEffect} from "react"
 
 function url(path){
-  return process.env.NODE_ENV = "development" ? `http://localhost:1234${path}` : path
+  return process.env.NODE_ENV === "development" ? `http://localhost:1234${path}` : path
 }
 
 function App() {
